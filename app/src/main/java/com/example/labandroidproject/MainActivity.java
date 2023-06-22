e8d3b0e8b0fd2b9fcffb934c3d1181a75de2e1e4
Aseel_W.D <aseel.aldeeek@gmail.com>
2023-06-22 22:42:34 +0300

    add signIn and signOut activities and did the UI for main activity

--- app/src/main/java/com/example/labandroidproject/MainActivity.java
+++ app/src/main/java/com/example/labandroidproject/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.labandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+// This is the main activity of the app. It is the first screen that the user sees when the app is launched.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button sign_in = (Button) findViewById(R.id.sign_in);
+        Button sign_up = (Button) findViewById(R.id.sign_up);
+
+        // open the sign in activity when the user clicks on the sign in button
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+
+        // open the sign up activity when the user clicks on the sign up button
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
