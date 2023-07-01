154ff628dfa8a2a482fe523e851d7c85d86c4bb6
Aseel_W.D <aseel.aldeeek@gmail.com>
2023-07-01 16:19:17 +0300

    add upload button

--- app/src/main/java/com/example/labandroidproject/Instructor_SignUp.java
+++ app/src/main/java/com/example/labandroidproject/Instructor_SignUp.java
@@ -2,13 +2,24 @@ package com.example.labandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
 
 public class Instructor_SignUp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor_sign_up);
+        // when click on the button load the instructor's profile picture
+        Button upload_photo = (Button) findViewById(R.id.upload);
+        upload_photo.setOnClickListener(view -> {
+            Intent iGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(iGallery, 1);
+        });
+
     }
 }
--- app/src/main/java/com/example/labandroidproject/Student_SignUP.java
+++ app/src/main/java/com/example/labandroidproject/Student_SignUP.java
@@ -2,7 +2,10 @@ package com.example.labandroidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
 
 public class Student_SignUP extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class Student_SignUP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_sign_up);
+        // when click on the button load the student's profile picture
+        Button upload_photo = (Button) findViewById(R.id.upload);
+        upload_photo.setOnClickListener(view -> {
+            Intent iGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(iGallery, 1);
+        });
     }
 }
