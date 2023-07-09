3cf3a2ec924a1d68cc3f57089b89a6e35fa0626a
top1million <1180620@student.birzeit.edu>
2023-07-09 23:49:08 +0300

    Merge remote-tracking branch 'origin/ahmad' into mergtest
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/labandroidproject/SignIn.java
    #	app/src/main/res/layout/activity_sign_in.xml

--- app/src/main/java/com/example/labandroidproject/Fragments/ThirdFragment.java
+++ app/src/main/java/com/example/labandroidproject/Fragments/ThirdFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.io.*;
 import androidx.fragment.app.Fragment;
@@ -18,7 +19,10 @@ public class ThirdFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+        View view = inflater.inflate(R.layout.fragment_third,container,false);
+        Button button = view.findViewById(R.id.button2);
+        Button button2 = view.findViewById(R.id.button3);
+        button.setOnClickListener();
+        return view;
     }
 }
