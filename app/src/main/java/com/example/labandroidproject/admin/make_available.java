e97e231aeec232de7edf22848e3694185eb943fa
Aseel_W.D <aseel.aldeeek@gmail.com>
2023-07-09 08:21:38 +0300

    add DataPicker

--- app/src/main/java/com/example/labandroidproject/admin/make_available.java
+++ app/src/main/java/com/example/labandroidproject/admin/make_available.java
@@ -1,5 +1,6 @@
 package com.example.labandroidproject.admin;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,8 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.labandroidproject.R;
@@ -29,7 +32,8 @@ import java.util.List;
 
 
 public class make_available extends Fragment {
-
+    TextView courseStartDate;
+    TextView registrationDeadline;
     public make_available() {
         // require a empty public constructor
     }
@@ -40,15 +44,40 @@ public class make_available extends Fragment {
         View v = inflater.inflate(R.layout.fragment_make_available, container, false);
         CollectionReference courses = FirebaseFirestore.getInstance().collection("courses");
         EditText instructorName = (EditText) v.findViewById(R.id.instructorName);
-        EditText registrationDeadline = (EditText) v.findViewById(R.id.registrationDeadline);
-        EditText courseStartDate = (EditText) v.findViewById(R.id.courseStartDate);
-        EditText courseVenue = (EditText) v.findViewById(R.id.courseVenue);
+        // EditText registrationDeadline = (EditText) v.findViewById(R.id.registrationDeadline);
+        Button insertStartDate = (Button) v.findViewById(R.id.courseStartDate);
+        courseStartDate = (TextView) v.findViewById(R.id.showText);
         Button makeAvailable = (Button) v.findViewById(R.id.makeAvailable);
+        registrationDeadline = (TextView) v.findViewById(R.id.showText_1);
+        Button insertEndDate = (Button) v.findViewById(R.id.registrationDeadline);
+        EditText courseVenue = (EditText) v.findViewById(R.id.courseVenue);
         List<String> courseNames = new ArrayList<>();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, courseNames);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
+
+
+        insertEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePicker=new DatePickerDialog(make_available.this.getActivity(), new DatePickerDialog.OnDateSetListener(){
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        registrationDeadline.setText(String.valueOf(day) +"/"+String.valueOf(month+1)+"/"+String.valueOf(year));
+                    }
+                }, 2023, 0, 15);
+
+                datePicker.show();
+            }
+        });
+
+        insertStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenDialog();
+            }
+        });
         courses.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -97,4 +126,16 @@ public class make_available extends Fragment {
         fragmentTransaction.replace(R.id.frameLayout,fragment);
         fragmentTransaction.commit();
     }
+
+    private  void OpenDialog(){
+        DatePickerDialog datePicker=new DatePickerDialog(this.getActivity(), new DatePickerDialog.OnDateSetListener(){
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                courseStartDate.setText(String.valueOf(day) +"/"+String.valueOf(month+1)+"/"+String.valueOf(year));
+            }
+        }, 2023, 0, 15);
+
+        datePicker.show();
+    }
+
 }
